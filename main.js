d17928d3302eac50fd080af7c8056b56558a7114
Martin <villie_tom2871@hotmail.com>
2022-12-11 22:43:55 -0600

    se integra menu-mobile al index.html y se agrega js para que funcione cuando se requiera

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuMobileIconBurguer = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toggleMenuDesktop);
+menuMobileIconBurguer.addEventListener('click',toggleMenuMobile);
 
 function toggleMenuDesktop(){
     desktopMenu.classList.toggle('inactive');
+};
+
+function toggleMenuMobile(){
+    menuMobile.classList.toggle('inactive')
 };
